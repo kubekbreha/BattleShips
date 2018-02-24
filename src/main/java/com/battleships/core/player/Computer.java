bc5039697c3710338d0aabd51e05c94ee6fce746
Jakub Brehuv <jakub.brehuv@student.tuke.sk>
2018-02-24 16:10:15 +0100

    assholed form hard level

--- src/main/java/com/battleships/core/player/Computer.java
+++ src/main/java/com/battleships/core/player/Computer.java
@@ -22,6 +22,7 @@ public class Computer extends Player {
     boolean moveLeft = false;
     boolean moveRight = false;
     boolean newLoop = false;
+    boolean shooted = false;
 
     public Computer() {
         rand = new Random();
@@ -136,144 +137,157 @@ public class Computer extends Player {
         int col = rand.nextInt(10);
         int row = rand.nextInt(10);
 
-        if(moveUp && newLoop){
-            if(savedRow - 2 > 0 && board[savedRow - 2][savedCol] != 1) {
-                Util.writeToBoard(board, 1, savedRow - 2, savedCol);
-                newLoop = false;
-            }else if(savedRow - 3 > 0 && board[savedRow - 3][savedCol] != 1) {
-                Util.writeToBoard(board, 1, savedRow - 3, savedCol);
-                newLoop = false;
-                moveUp = false;
-                moveDown = true;
-            }else{
-                moveUp = false;
-                moveDown = true;
+            if (moveUp && newLoop) {
+                if (savedRow - 2 > 0 && board[savedRow - 2][savedCol] != 1) {
+                    newLoop = false;
+                    Util.writeToBoard(board, 1, savedRow - 2, savedCol);
+                    shooted = true;
+                } else if (savedRow - 3 > 0 && board[savedRow - 3][savedCol] != 1) {
+                    newLoop = false;
+                    Util.writeToBoard(board, 1, savedRow - 3, savedCol);
+                    shooted = true;
+                    moveUp = false;
+                    moveDown = true;
+                } else {
+                    moveUp = false;
+                    moveDown = true;
+                }
             }
-        }
 
 
-        if(moveDown && newLoop){
-            if(savedRow + 2 < 10 && board[savedRow + 2][savedCol] != 1) {
-                Util.writeToBoard(board, 1, savedRow + 2, savedCol);
-                newLoop = false;
-            }else if(savedRow + 3 < 10 && board[savedRow + 3][savedCol] != 1) {
-                Util.writeToBoard(board, 1, savedRow + 3, savedCol);
-                newLoop = false;
-                moveDown = false;
-            }else{
-                moveDown = false;
+            if (moveDown && newLoop) {
+                if (savedRow + 2 < 10 && board[savedRow + 2][savedCol] != 1) {
+                    newLoop = false;
+                    Util.writeToBoard(board, 1, savedRow + 2, savedCol);
+                    shooted = true;
+                } else if (savedRow + 3 < 10 && board[savedRow + 3][savedCol] != 1) {
+                    newLoop = false;
+                    Util.writeToBoard(board, 1, savedRow + 3, savedCol);
+                    shooted = true;
+                    moveDown = false;
+                } else {
+                    moveDown = false;
+                }
             }
-        }
 
 
-        if(moveLeft && newLoop){
-            if(savedCol - 2 > 0 && board[savedRow][savedCol-2] != 1) {
-                Util.writeToBoard(board, 1, savedRow, savedCol-2);
-                newLoop = false;
-            }else if(savedCol - 3 > 0 && board[savedRow][savedCol-3] != 1) {
-                Util.writeToBoard(board, 1, savedRow, savedCol-3);
-                newLoop = false;
-                moveLeft = false;
-                moveRight = true;
-            }else{
-                moveLeft = false;
-                moveRight = true;
+            if (moveLeft && newLoop) {
+                if (savedCol - 2 > 0 && board[savedRow][savedCol - 2] != 1) {
+                    newLoop = false;
+                    Util.writeToBoard(board, 1, savedRow, savedCol - 2);
+                    shooted = true;
+                } else if (savedCol - 3 > 0 && board[savedRow][savedCol - 3] != 1) {
+                    newLoop = false;
+                    Util.writeToBoard(board, 1, savedRow, savedCol - 3);
+                    shooted = true;
+                    moveLeft = false;
+                    moveRight = true;
+                } else {
+                    moveLeft = false;
+                    moveRight = true;
+                }
             }
-        }
 
 
-        if(moveRight && newLoop){
-            if(savedCol + 2 < 0 && board[savedRow][savedCol+2] != 1) {
-                Util.writeToBoard(board, 1, savedRow, savedCol+2);
-                newLoop = false;
-            }else if(savedCol + 3 < 0 && board[savedRow][savedCol+3] != 1) {
-                Util.writeToBoard(board, 1, savedRow, savedCol+3);
-                newLoop = false;
-                moveRight = false;
-            }else{
-                moveRight = false;
+            if (moveRight && newLoop) {
+                if (savedCol + 2 < 0 && board[savedRow][savedCol + 2] != 1) {
+                    newLoop = false;
+                    Util.writeToBoard(board, 1, savedRow, savedCol + 2);
+                    shooted = true;
+                } else if (savedCol + 3 < 0 && board[savedRow][savedCol + 3] != 1) {
+                    newLoop = false;
+                    Util.writeToBoard(board, 1, savedRow, savedCol + 3);
+                    shooted = true;
+                    moveRight = false;
+                } else {
+                    moveRight = false;
+                }
             }
-        }
 
 
+            if(!shooted) {
+                if (makeCross && newLoop) {
+                    switch (orientation) {
+                        case 0:
+                            if (savedRow - 1 > 0) {
+                                if (board[savedRow - 1][savedCol] == 1) {
+                                    orientation = 90;
+                                    randomHard(board);
+                                }
+                                if (board[savedRow - 1][savedCol] == 3) {
+                                    moveUp = true;
+                                }
+                                Util.writeToBoard(board, 1, savedRow - 1, savedCol);
+                                orientation = 90;
+                            } else {
+                                orientation += 90;
+                                newLoop = true;
+                                randomHard(board);
+                            }
+                            break;
 
+                        case 90:
+                            if (savedCol + 1 < 10) {
+                                if (board[savedRow][savedCol + 1] == 1) {
+                                    orientation = 180;
+                                    randomHard(board);
+                                }
+                                if (board[savedRow][savedCol + 1] == 3) {
+                                    moveRight = true;
+                                }
+                                Util.writeToBoard(board, 1, savedRow, savedCol + 1);
+                                orientation = 180;
+                            } else {
+                                orientation += 90;
+                                newLoop = true;
+                                randomHard(board);
+                            }
+                            break;
 
-        if (makeCross && newLoop) {
-            switch (orientation) {
-                case 0:
-                    if (savedRow - 1 > 0) {
-                        if (board[savedRow - 1][savedCol] == 1) {
-                            orientation = 90;
-                            randomHard(board);
-                        }
-                        if (board[savedRow - 1][savedCol] == 3) {
-                            moveUp = true;
-                        }
-                        Util.writeToBoard(board, 1, savedRow - 1, savedCol);
-                        orientation = 90;
-                    } else {
-                        orientation += 90;
-                        randomHard(board);
+                        case 180:
+                            if (savedRow + 1 < 10) {
+                                if (board[savedRow + 1][savedCol] == 1) {
+                                    orientation = 270;
+                                    randomHard(board);
+                                }
+                                if (board[savedRow + 1][savedCol] == 3) {
+                                    moveDown = true;
+                                }
+                                Util.writeToBoard(board, 1, savedRow + 1, savedCol);
+                                orientation = 270;
+                            } else {
+                                orientation += 90;
+                                newLoop = true;
+                                randomHard(board);
+                            }
+                            break;
+
+                        case 270:
+                            if (savedCol - 1 > 0) {
+                                if (board[savedRow][savedCol - 1] == 1) {
+                                    orientation = 0;
+                                    makeCross = false;
+                                    randomHard(board);
+                                }
+                                if (board[savedRow][savedCol - 1] == 3) {
+                                    moveLeft = true;
+                                }
+                                Util.writeToBoard(board, 1, savedRow, savedCol - 1);
+                                orientation = 0;
+                                makeCross = false;
+                            } else {
+                                makeCross = false;
+                                orientation = 0;
+                                newLoop = true;
+                                randomHard(board);
+                            }
+                            break;
                     }
-                    break;
-
-                case 90:
-                    if (savedCol + 1 < 10) {
-                        if (board[savedRow][savedCol + 1] == 1) {
-                            orientation = 180;
-                            randomHard(board);
-                        }
-                        if (board[savedRow][savedCol + 1] == 3) {
-                            moveRight = true;
-                        }
-                        Util.writeToBoard(board, 1, savedRow, savedCol + 1);
-                        orientation = 180;
-                    } else {
-                        orientation += 90;
-                        randomHard(board);
-                    }
-                    break;
-
-                case 180:
-                    if (savedRow + 1 < 10) {
-                        if (board[savedRow + 1][savedCol] == 1) {
-                            orientation = 270;
-                            randomHard(board);
-                        }
-                        if (board[savedRow + 1][savedCol] == 3) {
-                            moveDown = true;
-                        }
-                        Util.writeToBoard(board, 1, savedRow + 1, savedCol);
-                        orientation = 270;
-                    } else {
-                        orientation += 90;
-                        randomHard(board);
-                    }
-                    break;
-
-                case 270:
-                    if (savedCol - 1 > 0) {
-                        if (board[savedRow][savedCol - 1] == 1) {
-                            orientation = 0;
-                            makeCross = false;
-                            randomHard(board);
-                        }
-                        if (board[savedRow][savedCol - 1] == 3) {
-                            moveLeft = true;
-                        }
-                        Util.writeToBoard(board, 1, savedRow, savedCol - 1);
-                        orientation = 0;
-                        makeCross = false;
-                    } else {
-                        makeCross = false;
-                        orientation = 0;
-                        randomHard(board);
-                    }
-                    break;
+                } else {
+                    basicRandomShoot(board, row, col, 'H');
+                }
             }
-        } else {
-            basicRandomShoot(board, row, col, 'H');
-        }
+
         newLoop = true;
 
     }
@@ -304,3 +318,169 @@ public class Computer extends Player {
         }
     }
 }
+
+
+//    private void randomHard(int[][] board) {
+//        int col = rand.nextInt(10);
+//        int row = rand.nextInt(10);
+//
+//        int writeCol = 0;
+//        int writeRow = 0;
+//
+//        if (moveUp && newLoop) {
+//            if (savedRow - 2 > 0 && board[savedRow - 2][savedCol] != 1) {
+//                newLoop = false;
+//                writeRow = savedRow - 2;
+//                writeCol = savedCol;
+//            } else if (savedRow - 3 > 0 && board[savedRow - 3][savedCol] != 1) {
+//                newLoop = false;
+//                writeRow = savedRow - 3;
+//                writeCol = savedCol;
+//                moveUp = false;
+//                moveDown = true;
+//            } else {
+//                moveUp = false;
+//                moveDown = true;
+//            }
+//        }
+//
+//
+//        if (moveDown && newLoop) {
+//            if (savedRow + 2 < 10 && board[savedRow + 2][savedCol] != 1) {
+//                newLoop = false;
+//                writeRow = savedRow + 2;
+//                writeCol = savedCol;
+//            } else if (savedRow + 3 < 10 && board[savedRow + 3][savedCol] != 1) {
+//                newLoop = false;
+//                writeRow = savedRow + 3;
+//                writeCol = savedCol;
+//                moveDown = false;
+//            } else {
+//                moveDown = false;
+//            }
+//        }
+//
+//
+//        if (moveLeft && newLoop) {
+//            if (savedCol - 2 > 0 && board[savedRow][savedCol - 2] != 1) {
+//                newLoop = false;
+//                writeRow = savedRow;
+//                writeCol = savedCol - 2;
+//            } else if (savedCol - 3 > 0 && board[savedRow][savedCol - 3] != 1) {
+//                newLoop = false;
+//                writeRow = savedRow;
+//                writeCol = savedCol - 3;
+//                moveLeft = false;
+//                moveRight = true;
+//            } else {
+//                moveLeft = false;
+//                moveRight = true;
+//            }
+//        }
+//
+//
+//        if (moveRight && newLoop) {
+//            if (savedCol + 2 < 0 && board[savedRow][savedCol + 2] != 1) {
+//                newLoop = false;
+//                writeRow = savedRow;
+//                writeCol = savedCol + 2;
+//            } else if (savedCol + 3 < 0 && board[savedRow][savedCol + 3] != 1) {
+//                newLoop = false;
+//                writeRow = savedRow;
+//                writeCol = savedCol + 3;
+//                moveRight = false;
+//            } else {
+//                moveRight = false;
+//            }
+//            newLoop = false;
+//        }
+//
+//
+//        if (makeCross && newLoop) {
+//            switch (orientation) {
+//                case 0:
+//                    if (savedRow - 1 > 0) {
+//                        if (board[savedRow - 1][savedCol] == 1) {
+//                            orientation = 90;
+//                            randomHard(board);
+//                        }
+//                        if (board[savedRow - 1][savedCol] == 3) {
+//                            moveUp = true;
+//                        }
+//                        writeRow = savedRow - 1;
+//                        writeCol = savedCol;
+//                        orientation = 90;
+//                    } else {
+//                        orientation += 90;
+//                        newLoop = true;
+//                        randomHard(board);
+//                    }
+//                    break;
+//
+//                case 90:
+//                    if (savedCol + 1 < 10) {
+//                        if (board[savedRow][savedCol + 1] == 1) {
+//                            orientation = 180;
+//                            randomHard(board);
+//                        }
+//                        if (board[savedRow][savedCol + 1] == 3) {
+//                            moveRight = true;
+//                        }
+//                        writeRow = savedRow;
+//                        writeCol = savedCol + 1;
+//                        orientation = 180;
+//                    } else {
+//                        orientation += 90;
+//                        newLoop = true;
+//                        randomHard(board);
+//                    }
+//                    break;
+//
+//                case 180:
+//                    if (savedRow + 1 < 10) {
+//                        if (board[savedRow + 1][savedCol] == 1) {
+//                            orientation = 270;
+//                            randomHard(board);
+//                        }
+//                        if (board[savedRow + 1][savedCol] == 3) {
+//                            moveDown = true;
+//                        }
+//                        writeRow = savedRow + 1;
+//                        writeCol = savedCol;
+//                        orientation = 270;
+//                    } else {
+//                        orientation += 90;
+//                        newLoop = true;
+//                        randomHard(board);
+//                    }
+//                    break;
+//
+//                case 270:
+//                    if (savedCol - 1 > 0) {
+//                        if (board[savedRow][savedCol - 1] == 1) {
+//                            orientation = 0;
+//                            makeCross = false;
+//                            randomHard(board);
+//                        }
+//                        if (board[savedRow][savedCol - 1] == 3) {
+//                            moveLeft = true;
+//                        }
+//                        writeRow = savedRow;
+//                        writeCol = savedCol - 1;
+//                        orientation = 0;
+//                        makeCross = false;
+//                    } else {
+//                        makeCross = false;
+//                        orientation = 0;
+//                        newLoop = true;
+//                        randomHard(board);
+//                    }
+//                    break;
+//            }
+//        } else {
+//            basicRandomShoot(board, row, col, 'H');
+//        }
+//
+//        newLoop = true;
+//
+//    }
