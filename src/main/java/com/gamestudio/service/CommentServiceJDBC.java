1d43599fe2552c252c2d4b0b3de5ffe6f631f349
kubekbreha <kubek.breha@gmail.com>
2018-03-11 22:54:59 +0100

    comment done

--- src/main/java/com/gamestudio/service/CommentServiceJDBC.java
+++ src/main/java/com/gamestudio/service/CommentServiceJDBC.java
@@ -2,6 +2,8 @@ package com.gamestudio.service;
 
 import com.gamestudio.entity.Comment;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /*
@@ -22,13 +24,49 @@ public class CommentServiceJDBC implements CommentService {
     public static final String USER = "root";
     public static final String PASSWORD = "Hackathon16";
 
+    public static final String INSERT_COMMENT =
+            "INSERT INTO comment (player, game, comment, commentedon) VALUES (?, ?, ?, ?)";
+
+    public static final String SELECT_COMMENT =
+            "SELECT player, game, comment, commentedon FROM score ORDER BY points DESC LIMIT 10;";
+
     @Override
     public void addComment(Comment comment) throws CommentException {
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            try(PreparedStatement ps = connection.prepareStatement(INSERT_COMMENT)){
+                ps.setString(1, comment.getGame());
+                ps.setString(2, comment.getPlayer());
+                ps.setString(3, comment.getComment());
+                ps.setDate(4, new Date(comment.getCommentedOn().getTime()));
 
+                ps.executeUpdate();
+            }
+        } catch (SQLException e) {
+            throw new ScoreException("Error saving  comment", e);
+        }
     }
 
     @Override
     public List<Comment> getComments(String game) throws CommentException {
-        return null;
+        List<Comment> comments = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            try(PreparedStatement ps = connection.prepareStatement(SELECT_COMMENT)){
+                ps.setString(1, game);
+                try(ResultSet rs = ps.executeQuery()) {
+                    while(rs.next()) {
+                        Comment comment = new Comment(
+                                rs.getString(1),
+                                rs.getString(2),
+                                rs.getString(3),
+                                rs.getTimestamp(4)
+                        );
+                        comments.add(comment);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            throw new ScoreException("Error loading comment", e);
+        }
+        return comments;
     }
 }
