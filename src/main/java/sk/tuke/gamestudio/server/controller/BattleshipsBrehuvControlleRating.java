09249f19d4f90b8724cd3667f5f8482554c076d5
Jakub Brehuv <kubek.breha@gmail.com>
2018-04-15 22:32:15 +0200

    login progress

--- src/main/java/sk/tuke/gamestudio/server/controller/BattleshipsBrehuvControlleRating.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/BattleshipsBrehuvControlleRating.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.WebApplicationContext;
+import sk.tuke.gamestudio.game.battleships.brehuv.core.util.DatabaseUtil;
+import sk.tuke.gamestudio.service.CommentService;
 import sk.tuke.gamestudio.service.RatingService;
 import sk.tuke.gamestudio.service.ScoreService;
 
@@ -18,10 +20,17 @@ public class BattleshipsBrehuvControlleRating {
     @Autowired
     private RatingService ratingService;
 
+    @Autowired
+    private CommentService commmentService;
+
     @RequestMapping("/battleships-brehuv-rating")
     public String mines(Model model) {
+        return "battleships-brehuv-rating";
+    }
 
-
+    @RequestMapping("/submitrating")
+    public String submit(String comment, Model model) {
+        DatabaseUtil.addComment(comment, commmentService);
         return "battleships-brehuv-rating";
     }
 }
