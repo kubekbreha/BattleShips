3c3b49050a887b843e1d0f184f6fdcba671f1f6a
Jakub Brehuv <jakub.brehuv@ibm.com>
2018-04-25 16:05:37 +0200

    add login register template files

--- src/main/java/sk/tuke/gamestudio/server/controller/BattleshipsBrehuvControllerUser.java
+++ src/main/java/sk/tuke/gamestudio/server/controller/BattleshipsBrehuvControllerUser.java
@@ -12,35 +12,39 @@ import sk.tuke.gamestudio.service.UserService;
 //http://localhost:8080/user
 @Controller
 @Scope(value = WebApplicationContext.SCOPE_SESSION)
-class BattleshipsBrehuvUserController {
+class BattleshipsBrehuvControllerUser {
     @Autowired
     private UserService userService;
 
     private User loggedUser;
+
 
     @RequestMapping("/user")
     public String user(Model model) {
         return "login";
     }
 
-    @RequestMapping("/login")
+
+    @RequestMapping("/battleships-brehuv-login")
     public String login(User user, Model model) {
-        user = userService.login(user.getUsername(), user.getPasswd());
-        loggedUser = user;
-        return "login";
+        //user = userService.login(user.getUsername(), user.getPasswd());
+        //loggedUser = user;
+        return "battleships-brehuv-login";
     }
 
-    @RequestMapping("/register")
+
+    @RequestMapping("/battleships-brehuv-register")
     public String register(User user, Model model) {
-        user = userService.register(user.getUsername(), user.getPasswd());
-        loggedUser = user;
-        return "login";
+        //user = userService.register(user.getUsername(), user.getPasswd());
+        //loggedUser = user;
+        return "battleships-brehuv-register";
     }
 
-    @RequestMapping("/logout")
+
+    @RequestMapping("/battleships-brehuv-logout")
     public String logout() {
         loggedUser = null;
-        return "login";
+        return "battleships-brehuv-gamemenu";
     }
 
     public User getLoggedUser() {
